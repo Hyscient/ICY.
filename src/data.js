0295fbb053518bf95ce1487eddc3e9697655e0d9
Hyscient <108424155+Hyscient@users.noreply.github.com>
2022-10-16 20:14:26 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1601762603339-fd61e28b698a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    title: "WINTER SALES",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    title: "POPULAR DEALS",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1550614000-4895a10e1bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "NEW ARRIVALS",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1571682262898-48532c58b3a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "GOWNS",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1613322800337-581785ea9f33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=683&q=80",
+    title: "JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0514/7941/2904/products/creamcrewneck_576x.jpg?v=1648245349",
+  },
+  {
+    id: 2,
+    img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/summer-shirts-for-men-1-1533739632.jpg?crop=1xw:1xh;center,top&resize=480:*",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://res.cloudinary.com/wolfandbadger/image/upload/f_auto,q_auto:best,c_pad,w_350,h_350/products/shesdytylxtnzsjgcrx0",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFnc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2Fwc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 7,
+    img: "https://cdn.shopify.com/s/files/1/0405/4705/7819/products/Enzo_Fly_Shirt-Shirts-2226-703-Light_Rose_8d6de2d9-85cc-487a-8245-c347275cef90_540x.jpg?v=1664269839",
+  },
+  {
+    id: 8,
+    img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-autumn-jackets-4-1567519565.jpg?crop=1xw:1xh;center,top&resize=480:*",
+  },
+];
